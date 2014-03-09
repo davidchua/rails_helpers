bea4dc71f3877272c8c6b103d244f362b1fd0ffc
David Chua <zhchua@gmail.com>
2014-03-09 19:43:56 +0800

    Humanize seconds into human readable hours/min/secs text

---
+++ application_helper.rb
@@ -0,0 +1,11 @@
+module ApplicationHelper
+  # http://stackoverflow.com/a/4136485/188357
+  def humanize secs
+    [[60, :seconds], [60, :minutes], [24, :hours], [1000, :days]].map{ |count, name|
+      if secs > 0
+        secs, n = secs.divmod(count)
+        "#{n.to_i} #{name}"
+      end
+    }.compact.reverse.join(' ')
+  end
+end
